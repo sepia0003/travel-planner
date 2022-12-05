29c40d7ddf2e6b188925ff4e6c74204291a943fc
sepia0003 <sepia0003@gmail.com>
2022-12-06 08:20:49 +0900

    refined

--- app/Models/dbModule.py
+++ app/Models/dbModule.py
@@ -5,21 +5,45 @@ class Database:
         self.conn = pymysql.connect(host='localhost', user='root', password='test1234', db='testdb', charset='utf8')
         self.cursor = self.conn.cursor(pymysql.cursors.DictCursor)
 
-    def execute(self, query, args={}):
-        self.cursor.execute(query, args)
-    
-    def execute_fetchone(self, query, args={}):
-        self.cursor.execute(query, args)
-        record = self.cursor.fetchone()
-        return record
-    
-    def execute_fetchall(self, query, args={}):
-        self.cursor.execute(query, args)
-        record = self.cursor.fetchall()
-        return record   
-    
-    def commit(self):
-        self.db.commit()
+    def makelocationtable(self):
+        query = '''
+            CREATE TABLE testtable(
+                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                lon FLOAT NOT NULL,
+                lat FLOAT NOT NULL,
+                util FLOAT NOT NULL,
+                open TIME NOT NULL,
+                close TIME NOT NULL
+            );
+        '''
+
+    def addlocation(self, location_info): #c
+        query = '''
+            INSERT INTO testtable(lon, lat, util, open, close)
+            VALUES(%s, %s, %s, %s, %s);
+        '''
+        self.cursor.execute(query, location_info)
+        self.conn.commit()
+
+    def getlocationlist(self): #r
+        query = '''
+            SELECT *
+            FROM testtable;
+        '''
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+
+        return result
+
+    def deletelocationlist(self): #d
+        query = '''
+            DELETE FROM testtable;
+        '''
+        self.cursor.execute(query)
+        self.conn.commit()
+       
+
+
 
 # Database의 객체만 만들면 서버생성,커서생성 자동으로해주고
 # 커서에명령어입력하는것만 따로 떼어낸 클래스임
