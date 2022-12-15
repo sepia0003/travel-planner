3033202d3d2219ec1a41a02d657e910bb65de434
sepia0003 <sepia0003@gmail.com>
2022-12-16 07:05:12 +0900

    update(gaModule_tw_md_alpha): Change the way of adding alpha
    because the existing way evaluated both one-error-node and mutiple-error-node with the same weight.

--- app/Models/gaModule_tw_md_alpha.py
+++ app/Models/gaModule_tw_md_alpha.py
@@ -128,7 +128,7 @@ class Tour:
                 else:
                     currenttime += frompoint.timeTo(topoint)
             if self.getnode(0).getopen() <= currenttime <= self.getnode(0).getclose() and not self.portion:
-                allutil += self.getnode(0).getutil()
+                allutil += self.getnode(0).getutil()    #돌아왔을때도 효용을 얻게해서 효용의 높은곳에서 시작하는 가능성이 높아지도록
             else:
                 pass
             self.tourutil = allutil
@@ -239,9 +239,10 @@ class GeneticAlgo:
         return newpopulation
 
     def cleanup(self, oldpopulation):
-        #oldpopulation의 tour1~tour6전부순회 해보보면서 tw에 안맞는 놈들의 노드리스트를 기록(중복없이 1세대당 1번만기록)
-        #기록한것의 알파를 1씩증가
-        alphaupnodes = []
+        #oldpopulation의 tour1~tour6전부순회 해보보면서 tw에 안맞는 노드가 있다면 badstackofnodes에 스택을 추가해나간다 
+        #(각노드는 투어에서 한번밖에 등장하지 않으므로 한노드의 스택의 최대값은 populationsize가 된다)
+        #그렇게 badstackofnodes가 만들어졌다면 populationsize/2 보다 많은 투어에서 tw를위반한 노드의 알파를 1씩증가시킨다
+        badstackofnodes = {} #노드:빈도 순으로 작성
         for i in range(0, oldpopulation.populationsize()):
             temptour = oldpopulation.gettour(i)
             currenttime = temptour.getnode(0).getopen() #pop을 구성하는 각 투어에서 맨앞에노드의open시간
@@ -256,16 +257,21 @@ class GeneticAlgo:
                     currenttime += frompoint.getstay()
                     currenttime += frompoint.timeTo(topoint)
                 else: #tw밖이면
-                    if frompoint not in alphaupnodes:
-                        alphaupnodes.append(frompoint)
+                    if frompoint not in badstackofnodes:
+                        badstackofnodes[frompoint] = 1
+                    else:
+                        badstackofnodes[frompoint] += 1
                     currenttime += frompoint.timeTo(topoint)
             if temptour.getnode(0).getopen() <= currenttime <= temptour.getnode(0).getclose():
                 pass #처음시작위치로 돌아왔을때 tw내부일때
             else: #tw밖일때
-                if temptour.getnode(0) not in alphaupnodes:
-                    alphaupnodes.append(temptour.getnode(0))
-        for ele in alphaupnodes:
-            ele.alpha += 1
+                if temptour.getnode(0) not in badstackofnodes:
+                    badstackofnodes[temptour.getnode(0)] = 1
+                else:
+                    badstackofnodes[temptour.getnode(0)] += 1
+        for ele in badstackofnodes.keys():
+            if badstackofnodes[ele] > oldpopulation.populationsize()/2:
+                ele.alpha += 1
         
         #GA객체만들때 입력한 최악수를 넘어가는 알파를 가진 노드를 nodestorage에서도, tour1~tour6에서도 삭제
         bannodes = []
@@ -297,8 +303,8 @@ class GeneticAlgo:
         child = Tour(self.nodestorage, False)
 
         #parent1이라는 하나의 투어 내부에서 가능한 모든 루트(시작점에돌아오지않는경로)를 스택앤캐치방식으로 잡고
-        #해당 루트의 피트니스값과 시작노드,끝노드의 인덱스를 딕셔너리로서 저장 
-        fitnessroutedict = {}
+        #해당 루트의 시작노드,끝노드의 인덱스를key로 피트니스값을value로 하는 딕셔너리로서 저장 
+        routefitnessdict = {}
         for portionstart in range(0, parent1.toursize()):
             historynode = []
             for commondiff in range(0, parent1.toursize()):
@@ -308,14 +314,19 @@ class GeneticAlgo:
                 historynode.append(parent1.getnode(portionend))
                 route = Tour(self.nodestorage, True)
                 route.tour = historynode
-                routefitness = route.getfitness()
-                fitnessroutedict[routefitness] = (portionstart, portionend) #portionstart,end는 parent1투어내부의 노드의인덱스
+                fitnessofroute = route.getfitness()
+                routefitnessdict[(portionstart, portionend)] = fitnessofroute #portionstart,end는 parent1투어내부의 노드의인덱스
         
         #그 딕셔너리에서 가장 피트니스가높은 루트를 parent1에서 가져온다.
         #그 루트의 시작노드, 끝노드의 인덱스가 오름차순이면 child에 그인덱스대로 복붙하고
         #내림차순이면 일단 시작노드의 인덱스부터 끝인덱스까지 child에 복붙하고 0인덱스부터 끝노드의 인덱스까지 child에 복붙
-        maxfitness = max(fitnessroutedict.keys())
-        parent1_patchinfo = fitnessroutedict[maxfitness]
+        maxfitness = max(routefitnessdict.values())
+        temprecord = []
+        for ele in routefitnessdict.keys():
+            if routefitnessdict[ele] == maxfitness:
+                temprecord.append(ele)
+        tempidx = int(len(temprecord) * random.random())
+        parent1_patchinfo = temprecord[tempidx]
         patchstart = parent1_patchinfo[0]
         patchend = parent1_patchinfo[1]
         if patchstart <= patchend:
@@ -340,7 +351,8 @@ class GeneticAlgo:
     def mutate(self, tour):
         for touridx1 in range(0, tour.toursize()):
             if random.random() < self.mutationrate:     # with a little probablility, exchange nodes in the tour
-                touridx2 = int(tour.toursize() * random.random())
+                touridx2 = int(tour.toursize() * random.random()) 
+                #random.random()은 0이상 1미만의 분수를 반환한다. 1이절대 될수없으므로 toursize를 곱해주고 int()로 소수점이하를 버리면 인덱스가 된다.
 
                 node1 = tour.getnode(touridx1)
                 node2 = tour.getnode(touridx2)
@@ -349,25 +361,58 @@ class GeneticAlgo:
                 tour.setnode(touridx1, node2)
 
 
+def confirmvalidity(sometour):
+    flag = 1 #1은 valid하다는의미 
+    currenttime = sometour.getnode(0).getopen() #min
+    for i in range(0, sometour.toursize()):
+        frompoint = sometour.getnode(i)
+        topoint = None
+        if i+1 < sometour.toursize():
+            topoint = sometour.getnode(i+1)
+        else:
+            topoint = sometour.getnode(0)
+        if frompoint.getopen() <= currenttime <= frompoint.getclose():
+            currenttime += frompoint.getstay()
+            currenttime += frompoint.timeTo(topoint)
+        else:
+            flag = 0
+            currenttime += frompoint.timeTo(topoint)
+    if sometour.getnode(0).getopen() <= currenttime <= sometour.getnode(0).getclose():
+        pass
+    else:
+        flag = 0
+    
+    if flag == 1:
+        validity = 'Valid!'
+    else:
+        validity = 'Fail!'
+    
+    print(validity)
+
+
 if __name__ == '__main__':
     n_nodes = 10
     populationsize = 50
-    n_generation = 100
-    worstnum = 90
+    n_generation = 700
+    worstnum = 100
 
     nodestorage = NodeStorage()
 
     # listing nodes
-    nodestorage.addnode(Node(lon=139.741424, lat=35.699721, util=3000000, stay=9, open=480, close=1020)) # TUS 8~17(hour)
-    nodestorage.addnode(Node(lon=139.728871, lat=35.661302, util=50, stay=9, open=480, close=1020)) # mori tower
-    nodestorage.addnode(Node(lon=139.714924, lat=35.643925, util=50, stay=9, open=480, close=1020)) # ebisu
-    nodestorage.addnode(Node(lon=139.701975, lat=35.682837, util=50, stay=9, open=480, close=1020)) # yoyogi
-    nodestorage.addnode(Node(lon=139.719525, lat=35.680659, util=50, stay=9, open=480, close=1020)) # shinanomachi
-    nodestorage.addnode(Node(lon=139.666109, lat=35.705378, util=50, stay=9, open=480, close=1020)) # nakano
-    nodestorage.addnode(Node(lon=139.668144, lat=35.661516, util=50, stay=9, open=480, close=1020)) # shimokitazawa
-    nodestorage.addnode(Node(lon=139.686511, lat=35.680789, util=50, stay=9, open=480, close=1020)) # gatsudai
-    nodestorage.addnode(Node(lon=139.579722, lat=35.702351, util=50, stay=9, open=480, close=1020)) # kichijoji
-    nodestorage.addnode(Node(lon=139.736571, lat=35.628930, util=50, stay=9, open=480, close=1020)) # shinagawa
+    # 주의, util이 너무 커버리면 그노드가 있는 투어의 fitness를구할때 twmiss나 distance의 마이너스값이 없는것이나 마찬가지가됨
+    # 결국 tw나 거리도 고려하지않고 오로지 투어의 util만을 가지고 투어들끼리의 비교를하게됨
+    nodestorage.addnode(Node(lon=139.741424, lat=35.699721, util=1, stay=9, open=480, close=1020)) # TUS 8~17(hour)
+    nodestorage.addnode(Node(lon=139.728871, lat=35.661302, util=5000, stay=9, open=0, close=10200)) # mori tower
+    nodestorage.addnode(Node(lon=139.714924, lat=35.643925, util=2, stay=9, open=480, close=1020)) # ebisu
+    nodestorage.addnode(Node(lon=139.701975, lat=35.682837, util=3, stay=9, open=480, close=1020)) # yoyogi
+    nodestorage.addnode(Node(lon=139.719525, lat=35.680659, util=5, stay=9, open=480, close=1020)) # shinanomachi
+    nodestorage.addnode(Node(lon=139.666109, lat=35.705378, util=10, stay=9, open=480, close=1020)) # nakano
+    nodestorage.addnode(Node(lon=139.668144, lat=35.661516, util=3, stay=9, open=480, close=1020)) # shimokitazawa
+    nodestorage.addnode(Node(lon=139.686511, lat=35.680789, util=6, stay=9, open=480, close=1020)) # gatsudai
+    nodestorage.addnode(Node(lon=139.579722, lat=35.702351, util=9, stay=9, open=480, close=1020)) # kichijoji
+    nodestorage.addnode(Node(lon=139.736571, lat=35.628930, util=10, stay=9, open=480, close=1020)) # shinagawa
+    nodestorage_map = NodeStorage()
+    nodestorage_map.storage = nodestorage.storage.copy()
     
     population = Population(nodestorage, populationsize=populationsize, init=True)
     geneticalgo = GeneticAlgo(nodestorage, worstnum)
@@ -379,25 +424,23 @@ if __name__ == '__main__':
             print(j.getalpha(), end='/')    #테스트
         print('\n')                         #테스트
 
-    result = population.getmostfittour().tour # result = [node, node, node, node, ...]
+    result = population.getmostfittour()
+    result_justlist = population.getmostfittour().tour # result = [node, node, node, node, ...]
+
+    # 그리고 나온 최종결과로 나온 루트가 진짜 tw상 갈수있는 루트인지 순회하면서 확인
+    confirmvalidity(result)
+
+    # 노드의 개수가 많아지면 제네레이션도 많아져야하나?
+    # 그럼 많아질때 worstnum도 증가시켜야하나?
+    # 노드개수 고정시켰을때에 대해서인데, 다양한 효용의 투어가 나오는데 여러개 해봐서 그중 좋은 투어를 반환하도록해야할까
+    
 
     #테스트(이하전부)########################################################################################################
     import matplotlib.pyplot as plt
 
-    nodestorage_original = NodeStorage()
-    nodestorage_original.addnode(Node(lon=139.741424, lat=35.699721, util=50, stay=9, open=480, close=1020)) # TUS 8~17(hour)
-    nodestorage_original.addnode(Node(lon=139.728871, lat=35.661302, util=50, stay=9, open=480, close=1020)) # mori tower
-    nodestorage_original.addnode(Node(lon=139.714924, lat=35.643925, util=50, stay=9, open=480, close=1020)) # ebisu
-    nodestorage_original.addnode(Node(lon=139.701975, lat=35.682837, util=50, stay=9, open=480, close=1020)) # yoyogi
-    nodestorage_original.addnode(Node(lon=139.719525, lat=35.680659, util=50, stay=9, open=480, close=1020)) # shinanomachi
-    nodestorage_original.addnode(Node(lon=139.666109, lat=35.705378, util=50, stay=9, open=480, close=1020)) # nakano
-    nodestorage_original.addnode(Node(lon=139.668144, lat=35.661516, util=50, stay=9, open=480, close=1020)) # shimokitazawa
-    nodestorage_original.addnode(Node(lon=139.686511, lat=35.680789, util=50, stay=9, open=480, close=1020)) # gatsudai
-    nodestorage_original.addnode(Node(lon=139.579722, lat=35.702351, util=50, stay=9, open=480, close=1020)) # kichijoji
-    nodestorage_original.addnode(Node(lon=139.736571, lat=35.628930, util=50, stay=9, open=480, close=1020)) # shinagawa
     original_lonlist = []
     original_latlist = []
-    for i in nodestorage_original.storage:
+    for i in nodestorage_map.storage:
         original_lonlist.append(i.getlon())
         original_latlist.append(i.getlat())
     plt.scatter(original_lonlist, original_latlist)
@@ -406,16 +449,22 @@ if __name__ == '__main__':
     
     lonlist = []
     latlist = []
-    for i in result:
+    for i in result_justlist:
         lonlist.append(i.getlon())
         latlist.append(i.getlat())
-    for i in range(0, len(result)):    
-        if i+1<len(result):
+    for i in range(0, len(result_justlist)):    
+        if i+1<len(result_justlist):
             plt.plot([lonlist[i], lonlist[i+1]], [latlist[i], latlist[i+1]], color="blue")
             plt.text(lonlist[i], latlist[i], '{}'.format(i))
         else:
             plt.plot([lonlist[i], lonlist[0]], [latlist[i], latlist[0]], color='red')
             plt.text(lonlist[i], latlist[i], '{}'.format(i))
+
+    resultutil = 0
+    for i in result.tour:
+        resultutil += i.getutil()
+    resultutil += result.tour[0].getutil()
+    plt.text(139.60, 35.64, '{}'.format(resultutil))
 
     plt.savefig('map(gaModlue_tw_md_alpha).png')
 
