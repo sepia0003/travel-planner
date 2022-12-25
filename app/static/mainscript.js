2b7232723143aae3d4af1f0d298a8ac56a692f32
sepia0003 <sepia0003@gmail.com>
2022-12-25 16:17:14 +0900

    update(*): Try to get result of gaModule asynchronously
    because it is inefficient to make new html files every single time we get new information.

--- app/static/mainscript.js
+++ app/static/mainscript.js
@@ -1,18 +1,34 @@
 function mask_screen(){
-    let targetarea = document.getElementsByTagName("body");
-
     let masking = document.createElement('div');
     masking.setAttribute('class', 'masking');
-    masking.setAttribute('style', 'width: 100%; height: 100%; background-color: #000000; opacity: 0.5; position: absolute; top: 0px; left: 0px; z-index: 9999;');
+    masking.setAttribute('style', 'width: 100%; height: 100%; background-color: #000000; opacity: 0.5; position: absolute; top: 0px; left: 0px; z-index: 9999; text-align: center;');
     masking.textContent = 'Calculating optimal tour';
 
     document.body.appendChild(masking)
 }
 
 function demask_screen(){
-    let targetarea = document.getElementsByTagName("body");
-    
     let masking = document.getElementsByClassName('masking')
 
     document.body.removeChild(masking)
+}
+
+function getresult_promise(){
+    return new Promise(function (resolve, reject){
+        // ajax를 통한 응답지도이미지
+        ajax('url',
+        function (response){
+            resolve(response);
+        });
+    });
+}
+
+async function getgaModuleresult(){
+    mask_screen();
+
+    let resultimage = await getresult_promise();
+
+    let sectiontag = document.getElementsByClassName('mapscreen')
+    sectiontag.appendChild(resultimage)
+    demask_screen();
 }
