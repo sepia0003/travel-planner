77b2d8798c7a805e850b74cd52b1eaff3a3133c7
sepia0003 <sepia0003@gmail.com>
2022-12-29 22:38:59 +0900

    update(*): Check whether starttime is empty
    because if start button is pressed with empty starttime, the existing code request server with empty starttime.

--- app/static/mainscript.js
+++ app/static/mainscript.js
@@ -1,8 +1,8 @@
 function mask_screen(){
-    let masking = document.createElement('div');
-    masking.setAttribute('class', 'masking');
-    masking.setAttribute('style', 'width: 100%; height: 100%; background-color: #000000; opacity: 0.5; position: absolute; top: 0px; left: 0px; z-index: 9999; text-align: center;');
-    masking.textContent = 'Calculating optimal tour';
+    let masking = document.createElement('div')
+    masking.setAttribute('class', 'masking')
+    masking.setAttribute('style', 'width: 100%; height: 100%; background-color: #000000; opacity: 0.5; position: absolute; top: 0px; left: 0px; z-index: 9999; text-align: center;')
+    masking.textContent = 'Calculating optimal tour'
 
     document.body.appendChild(masking)
 }
@@ -31,21 +31,25 @@ function demask_screen(){
 // }
 
 async function getgaModuleresult(){
-    mask_screen();
+    if (document.getElementById('starttime').value !== ""){
+        mask_screen()
 
-    let starttime = document.getElementsByClassName("starttime")
-    let data = {
-        starttime: starttime.value
+        let data = {
+            starttime: document.getElementById('starttime').value
+        }
+
+        let getresult = await fetch("http://localhost/searching", {
+            method : "POST",
+            headers : {
+                "Content-Type":"application/json;"
+            },
+            body : JSON.stringify(data)
+        })
+        .then(result=>'받은 이미지를 html에 띄우기')
+
+        demask_screen()
     }
-
-    let getresult = await fetch("http://localhost/searching", {
-        method : "POST",
-        headers : {
-            "Content-Type":"application/json;"
-        },
-        body : JSON.stringify(data)
-    })
-    .then(result=>'받은 이미지를 html에 띄우기')
-
-    demask_screen();
+    else{
+        document.getElementById('starttime').style.cssText = "border: 2px solid red;"
+    }
 }
